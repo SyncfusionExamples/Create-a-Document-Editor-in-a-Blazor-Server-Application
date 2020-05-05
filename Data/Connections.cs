97153f05f951cf1dfe9c755ab11cb1d31d03ba18
Arulraj A <arul.raj47@gmail.com>
2020-05-05 15:49:26 +0530

    Committed the example application.

---
+++ Data/Connections.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyBlazorServerApp
+{
+    public class Connections
+    {
+        public static string ConnectionString { get; set; } = "Your connection string";
+    }
+}
